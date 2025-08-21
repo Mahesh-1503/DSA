05317ea9bcd1c3d4c1c929f1bbe24cf1b73f4929
Mahesh-1503 <mnopxyz090@gmail.com>
2025-08-21 21:46:45 +0530

    *

---
+++ Python+DSA/ImportingCreatng_Modules_And_Packages/package/__init__.py
@@ -0,0 +1,12 @@
+'''
+__init__.py is a special Python file that indicates that the directory it is in should be treated as a package. It can be empty, or it can contain initialization code for the package.
+This file is executed when the package is imported, allowing you to set up package-level variables or import submodules.
+This file can also define the public API of the package by specifying which modules or functions should be accessible when the package is imported.
+For example, you can import specific functions or classes from submodules to make them available at the package level.
+This allows users to import them directly from the package without needing to know the internal structure of the package.
+'''
+
+'''
+In-Short __init__.py is a special file used in python to define packages and initialize their namespaces
+'''
+### Look at "Import_ModulesPackages.ipynb" file for more details on how to use this package
